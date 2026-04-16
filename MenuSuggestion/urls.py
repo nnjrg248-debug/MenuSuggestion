a0b25d22692567db6b141f38a603d171c045fe7e
nnjrg248-debug <nnjrg248@gmail.com>
2026-04-16 16:25:06 +0900

    04161625

--- MenuSuggestion/urls.py
+++ MenuSuggestion/urls.py
@@ -4,7 +4,8 @@ from django.urls import path,include
 urlpatterns=[
     path('admin/',admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')), 
-    path('',include('edit.urls')),
+    #path('',include('edit.urls')),
+    path('',include('Suggestion.urls')),
 
 
 ]
